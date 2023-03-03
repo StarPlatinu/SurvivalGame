5737e0dbb6664a3d4160eb9c94d9f7717f8b0d18
ThanhVu <thanhvp1509@gmail.com>
2023-03-03 23:55:12 +0700

    Map InProgress

---
+++ Survival game/Assets/Scripts/Reposition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+        {
+            return;
+        }
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        //Vector3 playerDir = GameManager.instance.player.transform.inputVec;
+
+    }
+}
