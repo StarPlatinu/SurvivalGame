ecddf7bd45d31c1949b561e0ee6d173f7df2e584
ThanhVu <thanhvp1509@gmail.com>
2023-03-03 22:46:54 +0700

    player flip left right

--- Survival game/Assets/Scripts/Player.cs
+++ Survival game/Assets/Scripts/Player.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //  uses the Rigidbody2D component to handle physics and movement
+    //a Vector2 that stores the input from the player for horizontal and vertical movement
     public Vector2 inputVec;
     public float speed;
-
+    Animator anim;
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +26,20 @@ public class Player : MonoBehaviour
         inputVec.x = Input.GetAxis("Horizontal");
         inputVec.y = Input.GetAxis("Vertical");
     }
-
+    //calculates the next movement vector 
     private void FixedUpdate()
 
     {
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + inputVec);
+        // moves the player to the next position 
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
