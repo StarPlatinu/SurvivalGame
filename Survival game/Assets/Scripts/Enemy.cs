5e6e34036472274bd44c7b81e18c299bfcb98800
buiquydangsnd1 <buiquydangsnd1@gmail.com>
2023-03-15 08:35:52 +0700

    update cm

--- Survival game/Assets/Scripts/Enemy.cs
+++ Survival game/Assets/Scripts/Enemy.cs
@@ -49,7 +49,7 @@ public class Enemy : MonoBehaviour
         health = maxHealth;
     }
     //buidang update code
-    //buidang comment
+
     public void Init(SpawnData data)
     {
         anim.runtimeAnimatorController = animCon[data.spriteType];
