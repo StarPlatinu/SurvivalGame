a63ed951564fa7ed68e3226526e78686bb607ac9
Vu Linh Duc (K15 HL) <@ducvlhe153739>
2023-03-17 00:16:37 +0700

    enemy dead

--- Survival game/Assets/Scripts/GameManager.cs
+++ Survival game/Assets/Scripts/GameManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
-
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 20 * 10f;
+    [Header("# player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
 
     private void Awake()
     {
@@ -24,5 +30,16 @@ public class GameManager : MonoBehaviour
             gameTime = maxGameTime;
         }
 
+    }
+
+    public void GetExp()
+    {
+        exp++;
+
+        if(exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
+        }
     }
 }
