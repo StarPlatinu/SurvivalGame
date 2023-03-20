a0dfde4906b1ab1df06d35a871a7a5bb91948277
StarPlatinu <thanhvp1509@gmail.com>
2023-03-20 18:03:48 +0700

    timer

--- Survival game/Assets/Scripts/HUD.cs
+++ Survival game/Assets/Scripts/HUD.cs
@@ -36,8 +36,11 @@ public class HUD : MonoBehaviour
                 myText.text = string.Format("{0:F0}", GameManager.instance.kill);
                 break;
             case InfoType.Time:
-
-                break;
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
+                break;             
             case InfoType.Health:
 
                 break;
